ae08558698a183fb8983ff437c298a2bc7bc31f2
loovjo <jonathan.loov@gmail.com>
2020-09-17 22:49:50 +0200

    More instructions

--- human-cpu-server/vm/main.py
+++ human-cpu-server/vm/main.py
@@ -22,3 +22,6 @@ def display_instinfo(code):
 # Add ra 0x1234 0x4321
 display_instinfo(b"+AN\x12\x34\x00\x00\x00\x00\x00\x00N\x43\x21\x00\x00\x00\x00\x00\x00")
 
+# Set ra 0x5577
+display_instinfo(b"SAN\x12\x34\x00\x00\x00\x00\x00\x00")
+
