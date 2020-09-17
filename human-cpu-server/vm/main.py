1edbfd7e98ca59920023b048b779667fd8d93285
loovjo <jonathan.loov@gmail.com>
2020-09-17 22:39:41 +0200

    Descriptions

--- human-cpu-server/vm/main.py
+++ human-cpu-server/vm/main.py
@@ -4,13 +4,12 @@ def display_instinfo(code):
     virt = vm.VirtualMachine(code)
 
     instructions = virt.query_instructions()
-    print(len(instructions), "available")
 
     for inst in instructions:
         print("==")
-        print(inst)
-        print(inst.get_desc())
-        print(inst.fake_action(virt.cores[0]))
+        print("Instruction:", inst)
+        print("Human description:", inst.get_desc())
+        print("Wanted action:", inst.fake_action(virt.cores[0]))
 
 # Add ra 0x1234 0x4321
 display_instinfo(b"+AN\x12\x34\x00\x00\x00\x00\x00\x00N\x43\x21\x00\x00\x00\x00\x00\x00")
