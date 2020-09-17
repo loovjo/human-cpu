f63c074d8a5d0bc8bbd6e2396521cc3a55b6380c
loovjo <jonathan.loov@gmail.com>
2020-09-17 22:33:47 +0200

    Show some info about every instruction

--- human-cpu-server/vm/main.py
+++ human-cpu-server/vm/main.py
@@ -5,4 +5,10 @@ CODE = open("test-code.bin", "rb").read()
 virt = vm.VirtualMachine(CODE)
 
 instructions = virt.query_instructions()
-print(instructions)
+print(len(instructions), "available")
+
+for inst in instructions:
+    print("==")
+    print(inst)
+    print(inst.get_desc())
+    print(inst.fake_action(virt.cores[0]))
