e8831fdee483704bd015988bb86425b7305bec46
loovjo <jonathan.loov@gmail.com>
2020-09-17 19:59:44 +0200

    Rewrite the simple example

--- human-cpu-server/assembly-files/simple.asm
+++ human-cpu-server/assembly-files/simple.asm
@@ -1,7 +1,34 @@
-_start:
-    Set 0 65
-    Set 1 66
-    Set 2 67
-    Set 3 68
+'start
 
-    print 4 0
+    Make-handler `on_child_created` `1` `0x1000` `on_child_created`
+
+    Create-actor `run_a` $ra
+
+    Idle
+
+
+'on_child_created
+
+    ReadMem $ra `0x1000`
+
+    SetMem `0x1008` `0x69`
+    SetMem `0x1009` `0x6a`
+
+    Send-msg $ra `get_msg` `0x2` `0x1008`
+
+
+'run_a
+    Make-handler `get_msg` `2` `0x1000` `get_msg`
+
+    Self-addr $rx
+    SetMem `0x1000` $rx
+
+    Send-msg $ra `on_child_created` `1` `0x1000`
+
+    Idle
+
+
+'get_msg
+
+    ReadMem $rx `0x1000`
+    Print $rx `0x1001`
